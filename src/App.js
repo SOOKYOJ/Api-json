eb6908d78b2954464433b478680b5a29b0941ee7
SOOKYOJ <sookyo0810@naver.com>
2024-01-02 19:41:37 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import ListGroup from 'react-bootstrap/ListGroup';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function List(props) {
+  return (
+    <ListGroup>
+      <ListGroup.Item>{props.name}</ListGroup.Item>
+      <ListGroup.Item>{props.username}</ListGroup.Item>
+      <ListGroup.Item>{props.email}</ListGroup.Item>
+      <ListGroup.Item>{props.phone}</ListGroup.Item>
+      <ListGroup.Item>{props.website}</ListGroup.Item>
+    </ListGroup>
+  );
+}
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users~')
+      .then((response) => response.json())
+      .then((json) => setUsers(json));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {users.map((user) => (
+        <List name={user.name} username={user.username} email={user.email} phone={user.phone} website={user.website} />
+      ))}
     </div>
   );
 }
