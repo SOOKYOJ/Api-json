3a9789c61263bbcbfc7f97042d6dfa3627b361d2
SOOKYOJ <sookyo0810@naver.com>
2024-01-02 19:49:26 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/users~')
+    fetch('https://jsonplaceholder.typicode.com/users')
       .then((response) => response.json())
       .then((json) => setUsers(json));
   }, []);
